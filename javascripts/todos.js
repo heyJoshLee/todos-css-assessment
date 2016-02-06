8eb69872218e8bbd32a0844f1dc269584296fc92
jleewebdev <lee45276@gmail.com>
2016-02-06 15:35:41 +0800

    Refactored html, css. Commened html, css, js. Changed classes to ids

--- javascripts/todos.js
+++ javascripts/todos.js
@@ -1,37 +1,33 @@
 $(function() {
 
 
+/////////////
 // Events
+/////////////
 
-  // hightlight current todo
 
-  $(".all-todos").on("click", "li", function(e) {
+// hightlight current todo
+  $("#all-todos").on("click", "li", function(e) {
     e.preventDefault();
     var $this = $(this);
     $this.closest("ul").find(".active").removeClass("active");
     $(this).addClass("active");
   });
 
-  // mark as complete
-  // toggle class would add class, but not remove class
-  $(".current-todo").on("click", "label", function(e) {
-    var $this = $(this);
-      $this.parent("li").addClass("complete");
-
-  });
-
-
-
   // Toggle menu
-  $("#menu-icon").on("click", function() {
+  $("#menu-icon").on("click", function(e) {
+    e.preventDefault();
     $("#menu").animate({width: "toggle"});
   });
 
-  $(".close-button, .modal-bg").on("click", function() {
+  // Close modal
+  $("#close-button, .modal-bg").on("click", function(e) {
+    e.preventDefault();
     $(".modal, .modal-bg").fadeOut();
   });
 
-  $(".open-button").on("click", function(e) {
+  // Open modal
+  $("#open-button").on("click", function(e) {
     e.preventDefault();
     $(".modal, .modal-bg").fadeIn();
   });

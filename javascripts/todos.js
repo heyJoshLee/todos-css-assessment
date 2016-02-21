6a95eeafd07be9080db315d9182f2a3a82a80237
jleewebdev <lee45276@gmail.com>
2016-02-21 18:27:45 +0800

    Checkbox changes state

--- javascripts/todos.js
+++ javascripts/todos.js
@@ -1,12 +1,10 @@
 $(function() {
 
-
 /////////////
 // Events
 /////////////
 
-
-// hightlight current todo
+// hightlight date
   $("#all-todos").on("click", "li", function(e) {
     e.preventDefault();
     var $this = $(this);
@@ -14,11 +12,10 @@ $(function() {
     $(this).addClass("active");
   });
 
-  // Toggle menu
+  // Toggle date_menu
   $("#menu-icon").on("click", function(e) {
     e.preventDefault();
     var $menu = $("#menu");
-
     $menu.animate({width: "toggle"});
     //$("#menu").fadeToggle();
     //$("#menu").toggleClass("hide");
@@ -27,29 +24,42 @@ $(function() {
   // Close modal
   $(document).on("click", "#close_button, #modal_bg", function(e) {
     e.preventDefault();
-    $("#modal, #modal_bg").fadeOut();
+    fadeOutModal();
   });
 
+  // Show input to change date
   $("#modal_container").on("dblclick", ".add_date", function(e) {
     e.preventDefault();
     var $this = $(this);
     $this.html(templates.edit_date(current_todo));
-    console.log("Come add the date");
   });
 
+  // Save Todo Date
   $("#modal_container").on("click", ".save", function(e) {
     e.preventDefault()
-    var $this = $(this);
-    var new_value = $(".add_date > input").val();
-    console.log(new_value)
-    find_where("id", current_todo.id, todos).date = new_value ;
+    var $this = $(this),
+        new_value = $(".add_date > input").val();
+
+    findWhere("id", current_todo.id, todos).date = new_value ;
     $("#modal_container").html(templates.modal(current_todo));
-    $("#modal, #modal_bg").fadeOut();
+    fadeOutModal();
     render();
   });
 
+  // Mark Todo as complete in modal
+  $("#modal_container").on("click", ".mark_as_complete", function(e) {
+    e.preventDefault();
+    var $id = $("#modal").attr("data-id"),
+        item = findWhere("id", $id, todos);
+     item.checked = !item.checked;
+    $("#current_todos").find("#" + $id).attr("checked", "checked");
+    saveLocalStorage();
+    fadeOutModal();
+    render();
+    console.log(item);
+  });
 
-  // Add button
+  // Add new Todo
   $("#current_todos").on("click", "#add_button", function(e) {
     e.preventDefault();
     var input = $("#new_todo_input").val();
@@ -57,30 +67,40 @@ $(function() {
     render();
   });
 
+  // Show modal
   $("#current_todos").on("click", ".modal_launcher", function(e) {
     var $id = $(this).next(".trash_can").attr("data-id");
     $("#modal, #modal_bg").fadeIn();
-    current_todo = find_where("id", $id, todos);
-    console.log("current todo:  " + current_todo.name);
+    current_todo = findWhere("id", $id, todos);
     showModal(current_todo);
-  })
+  });
 
+  // Delete Todo
   $("#current_todos").on("click", ".trash_can", function(e) {
     e.preventDefault();
-    var id = $(this).attr("data-id");
-    var item_to_delete = find_where("id", id, todos);
-    var spot = todos.indexOf(item_to_delete);
+    var id = $(this).attr("data-id"),
+        item_to_delete = findWhere("id", id, todos),
+        spot = todos.indexOf(item_to_delete);
     if (spot === -1) {
       alert("can't find");
       return;
     }
     todos.splice(todos.indexOf(item_to_delete), 1);
-    console.log("id to delete: " + id);
-    console.log(todos);
     render();
   });
 
-  function find_where(key, value, collection) {
+  // Check box changes 'checked' attribute of Todo
+  $("#current_todos").on("click", "[type='checkbox']", function(e) {
+    var $this = $(this),
+        $id = $this.attr("data-id"),
+        item = findWhere("id", $id, todos);
+        item.checked = !item.checked;
+        console.log(item);
+
+  });
+
+  // Find object in collection
+  function findWhere(key, value, collection) {
     var match;
     for(var i = 0; i < collection.length; i++) {
       if (collection[i][key] === +value) {
@@ -104,27 +124,29 @@ $(function() {
 
   function Todo(params) {
       Todo.created++;
-      localStorage.setItem("Todo_created", Todo.created);
       this.name = params.name;
       this.date = undefined;
       this.id = Todo.created;
       this.body = "";
+      this.checked = false;
       todos.push(this);
+      saveLocalStorage();
       console.log("Created todos: " + Todo.created);
   }
 
 
-
+  // Compile Handlebars tempaltes
   $("[type='x-handlebars-template']").each(function(template) {
     var $template = $(this);
     templates[$template.attr("id")] = Handlebars.compile($template.html());
   });
 
+  // Render HTML in #current_todos
   function render() {
     $("#current_todos").html(templates.current_todos({todos: todos}));
-    localStorage.setItem("todos", JSON.stringify(todos));
   }
 
+  // Load information from localStorage
   function loadTodos() {
     if (localStorage.getItem("todos")) {
       todos = JSON.parse(localStorage.getItem("todos"));
@@ -135,16 +157,25 @@ $(function() {
     }
   }
 
+  // Save items to localStorage
+  function saveLocalStorage() {
+    localStorage.setItem("todos", JSON.stringify(todos));
+    localStorage.setItem("Todo_created", Todo.created);
+  }
+
+  // Load page
   function init() {
     loadTodos();
     render();
-    console.log("rendered!");
-    console.log(todos);
   }
 
+  // Fill in Modal
   function showModal(current_todo) {
     $("#modal_container").html(templates.modal(current_todo));
-    console.log("fill in modal with " + current_todo);
+  }
+
+  function fadeOutModal() {
+    $("#modal, #modal_bg").fadeOut();
   }
 
 
